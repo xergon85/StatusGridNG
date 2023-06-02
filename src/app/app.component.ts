c173a3cb81bb3f7c0ae0df5d115508f83eb25e6e
xergon85 <christoffer.isenberg@gmail.com>
2023-06-02 09:49:13 +0200

    Update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GridConfiguration } from './models/grid-configuration';
+import { GridConfigurationService } from './services/grid-configuration.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StatusGridNG';
+
+  constructor(private gridConfigurationService: GridConfigurationService) {}
+
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditGridComponent } from './components/edit-grid/edit-grid.component';
+import { ConfigurationListComponent } from './components/configuration-list/configuration-list.component';
+import { GridCellComponent } from './components/grid-cell/grid-cell.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditGridComponent,
+    ConfigurationListComponent,
+    GridCellComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
